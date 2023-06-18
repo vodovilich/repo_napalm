980c4f3693e288cb3f393d3c28a3e44973594848
Gandalf <vodovilich@gmail.com>
2023-06-18 11:57:57 -0400

    add file 04 - pinging

---
+++ 04_napalm_ping.py
@@ -0,0 +1,8 @@
+import json
+from napalm import get_network_driver
+driver = get_network_driver('ios')
+iol_01 = driver('192.168.100.13', 'gandalf', 'grey')
+iol_01.open()
+
+ios_output = iol_01.ping('8.8.8.8')
+print(json.dumps(ios_output, indent=4))
