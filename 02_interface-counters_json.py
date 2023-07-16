9977a2335d171bbf5ce74b74f210d119de152c84
Gandalf <vodovilich@gmail.com>
2023-07-16 04:11:16 -0400

    add output to 02

--- 02_interface-counters_json.py
+++ 02_interface-counters_json.py
@@ -9,3 +9,21 @@ print(json.dumps(ios_output, indent=4, sort_keys=True))
 
 ios_output = iol_01.get_interfaces()
 print(json.dumps(ios_output, indent=4))
+
+"""
+{
+    "Ethernet0/0": {
+        "rx_broadcast_packets": 1969,
+        "rx_discards": 0,
+        "rx_errors": 0,
+        "rx_multicast_packets": 8,
+        "rx_octets": 231102,
+        "rx_unicast_packets": 2519,
+        "tx_broadcast_packets": -1,
+        "tx_discards": 0,
+        "tx_errors": 0,
+        "tx_multicast_packets": -1,
+        "tx_octets": 164978,
+        "tx_unicast_packets": 1340
+    },
+"""
