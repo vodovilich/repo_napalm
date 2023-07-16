f26a254bc86cefbd593d542ef8169683057d4f20
Gandalf <vodovilich@gmail.com>
2023-07-16 17:31:08 -0400

    i am drunk, fuck off. NAPALM course is completed, go on with CBT course. Si peder. Jebao te majku u usta

--- 07_napalm_conf-cmd.py
+++ 07_napalm_conf-cmd.py
@@ -3,10 +3,17 @@ from napalm import get_network_driver
 driver = get_network_driver('ios')
 
 #ISO-XE REQUIRED!!! won't work on IOS
-iol_01 = driver('192.168.100.7', 'gandalf', 'grey')
-iol_01.open()
 
-print("Connecting to: " + "192.168.100.7")
-iol_01.load_merge_candidate(config='logging buffered 16500')
-iol_01.commit_config()
-iol_01.close()
+
+iplist = ['192.168.100.7']
+for ip_host in iplist:
+    print("Connecting to: " + str(ip_host))
+    iol_01 = driver(ip_host, 'gandalf', 'grey')
+    iol_01.open()
+    iol_01.load_merge_candidate(filename='07_commands.txt')
+    iol_01.commit_config()
+    iol_01.close()
+    
+    
+#FOR ONLY ONE COMMAND use CONFIG argument of load_merge_candidate method
+# iol_01.load_merge_candidate(config='logging buffered 16501')
--- 08_list-conf.py
+++ 08_list-conf.py
@@ -4,9 +4,9 @@ driver = get_network_driver('ios')
 iol_01 = driver('192.168.100.12', 'gandalf', 'grey')
 iol_01.open()
 
-print("Connecting to: " + "192.168.100.12")
+print("Connecting to: " + "192.168.100.250")
 
-iol_01.load_merge_candidate(filename='05_commands.cfg')
+iol_01.load_merge_candidate(filename='08_commands.cfg')
 
 diffs = iol_01.compare_config()
 if len(diffs) > 0:
@@ -16,3 +16,35 @@ else:
     print('No changes required')
     iol_01.discard_config()
 iol_01.close()
+
+"""
+ERROR RETURNED on CSR1000v:
+Connecting to: 192.168.100.7
+Traceback (most recent call last):
+  File "/home/gandalf/PythonDir/repo_napalm/08_list-conf.py", line 9, in <module>
+    iol_01.load_merge_candidate(filename='07_commands.txt')
+  File "/home/gandalf/PythonDir/repo_napalm/napalm-venv/lib/python3.9/site-packages/napalm/ios/ios.py", line 319, in load_merge_candidate
+    return_status, msg = self._load_candidate_wrapper(
+  File "/home/gandalf/PythonDir/repo_napalm/napalm-venv/lib/python3.9/site-packages/napalm/ios/ios.py", line 286, in _load_candidate_wrapper
+    (return_status, msg) = self._scp_file(
+  File "/home/gandalf/PythonDir/repo_napalm/napalm-venv/lib/python3.9/site-packages/napalm/ios/ios.py", line 735, in _scp_file
+    return self._xfer_file(
+  File "/home/gandalf/PythonDir/repo_napalm/napalm-venv/lib/python3.9/site-packages/napalm/ios/ios.py", line 791, in _xfer_file
+    if not transfer.verify_space_available():
+  File "/home/gandalf/PythonDir/repo_napalm/napalm-venv/lib/python3.9/site-packages/netmiko/scp_handler.py", line 213, in verify_space_available
+    space_avail = self.remote_space_available(search_pattern=search_pattern)
+  File "/home/gandalf/PythonDir/repo_napalm/napalm-venv/lib/python3.9/site-packages/netmiko/scp_handler.py", line 159, in remote_space_available
+    raise ValueError(msg)
+ValueError: pattern: (\d+) \w+ free not detected in output:
+
+Directory of system:/
+
+  758  drwx           0                    <no date>  cme
+    2  -r--           0                    <no date>  default-running-config
+    3  drwx           0                    <no date>  its
+  105  dr-x           0                    <no date>  memory
+    1  -rw-        1811                    <no date>  running-config
+  104  dr-x           0                    <no date>  vfiles
+
+No space information available
+"""
