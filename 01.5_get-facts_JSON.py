5bc6e8adedf021a7d04b20ce863ff1a7cbbb68cf
vodovilich <107003263+vodovilich@users.noreply.github.com>
2024-09-22 15:07:33 +0200

    Update 01-5_get-facts_JSON.py
    update

--- 01.5_get-facts_JSON.py
+++ 01.5_get-facts_JSON.py
@@ -1,7 +1,3 @@
-"""
-returns a dicrionary:
-{'uptime': 780.0, 'vendor': 'Cisco', 'os_version': 'Linux Software (I86BI_LINUX-ADVENTERPRISEK9-M), Version 15.7(3)M0.1, DEVELOPMENT TEST SOFTWARE', 'serial_number': '67108880', 'model': 'Unknown', 'hostname': 'iol-rtr157-01', 'fqdn': 'iol-rtr157-01.atffc.hui', 'interface_list': ['Ethernet0/0', 'Ethernet0/1', 'Ethernet0/2', 'Ethernet0/3']}
-"""
 import json
 from napalm import get_network_driver
 driver = get_network_driver('ios')
@@ -10,3 +6,15 @@ iol_01.open()
 
 ios_output = iol_01.get_facts()
 print (json.dumps(ios_output, indent=4))
+
+"""
+{'fqdn': 'iol-157-01.atffk.hui',
+ 'hostname': 'iol-157-01',
+ 'interface_list': ['Ethernet0/0', 'Ethernet0/1', 'Ethernet0/2', 'Ethernet0/3'],
+ 'model': 'Unknown',
+ 'os_version': 'Linux Software (I86BI_LINUX-ADVENTERPRISEK9-M), Version '
+               '15.7(3)M0.1, DEVELOPMENT TEST SOFTWARE',
+ 'serial_number': '12345678',
+ 'uptime': 2880.0,
+ 'vendor': 'Cisco'}
+ """
