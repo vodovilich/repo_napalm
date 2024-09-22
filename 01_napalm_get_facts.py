e8752a13e7556c97ef168118f61c88d137252596
vodovilich <107003263+vodovilich@users.noreply.github.com>
2024-09-22 15:01:23 +0200

    Update 01_napalm_get_facts.py
    update

--- 01_napalm_get_facts.py
+++ 01_napalm_get_facts.py
@@ -1,11 +1,21 @@
-"""
-returns a dicrionary:
-{'uptime': 780.0, 'vendor': 'Cisco', 'os_version': 'Linux Software (I86BI_LINUX-ADVENTERPRISEK9-M), Version 15.7(3)M0.1, DEVELOPMENT TEST SOFTWARE', 'serial_number': '67108880', 'model': 'Unknown', 'hostname': 'iol-rtr157-01', 'fqdn': 'iol-rtr157-01.atffc.hui', 'interface_list': ['Ethernet0/0', 'Ethernet0/1', 'Ethernet0/2', 'Ethernet0/3']}
-"""
 from napalm import get_network_driver
+import pprint
+
 driver = get_network_driver('ios')
-iol_01 = driver('192.168.100.13', 'gandalf', 'grey')
+iol_01 = driver('192.168.100.8', 'gandalf', 'grey')
 iol_01.open()
 
-ios_output = iol_01.get_facts()
-print(ios_output)
+output = iol_01.get_facts()
+pprint.pprint(output)
+
+"""
+{'fqdn': 'iol-157-01.atffk.hui',
+ 'hostname': 'iol-157-01',
+ 'interface_list': ['Ethernet0/0', 'Ethernet0/1', 'Ethernet0/2', 'Ethernet0/3'],
+ 'model': 'Unknown',
+ 'os_version': 'Linux Software (I86BI_LINUX-ADVENTERPRISEK9-M), Version '
+               '15.7(3)M0.1, DEVELOPMENT TEST SOFTWARE',
+ 'serial_number': '12345678',
+ 'uptime': 2340.0,
+ 'vendor': 'Cisco'}
+"""
